df9190b5b3b2e120ef4e0b00d770a3f97e18dccc
everestp <paudeleverest09@gmail.com>
2025-12-23 17:50:55 +0545

    45 .Modified the broker service to handle mail

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+
 	"net/http"
 )
 
@@ -12,6 +13,14 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 type AuthPayload struct {
@@ -45,6 +54,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
 		app.logItem(w, requestPayload.Log)
+	case "mail":
+		app.SendMail(w, requestPayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
@@ -127,5 +138,38 @@ func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
 	var payload jsonResponse
 	payload.Error = false
 	payload.Message = "logged"
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) SendMail(w http.ResponseWriter, msg MailPayload) {
+	jsonData, _ := json.MarshalIndent(msg, "", "\t")
+
+	//call the mail service
+	mailServiceURL := "http://mailer-service/send"
+	// post to mail service
+	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back the risght status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("Error calling mailer service"))
+		return
+	}
+
+	//send back json
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Messege sent to" + msg.To
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
